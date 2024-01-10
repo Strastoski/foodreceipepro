99125363945510c9bd5bd8056b2165c5c1a71d41
Strastoski <ezrawilliam98@gmail.com>
2024-01-10 22:43:01 +0700

    DetailRecipe 20%

--- src/Page/DetailRecipe.jsx
+++ src/Page/DetailRecipe.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../components/NavNonLog";
+import bgfood from "../Image/food.png";
 
 const DetailRecipe = () => {
   return (
@@ -9,6 +10,24 @@ const DetailRecipe = () => {
       {/* End Nav */}
 
       <h1 className="detail-title">LOREAM SANDWICH</h1>
+
+      <div className="pict-container">
+        <div
+          style={{
+            position: "absolute",
+            top: 240,
+            left: auto,
+            display: "flex",
+            justifyContent: "space-evenly",
+            backgroundImage: `url(${bgfood})`,
+            backgroundPosition: "center",
+            backgroundSize: "cover",
+            width: 380,
+            height: 380,
+            zIndex: 1,
+          }}
+        />
+      </div>
     </div>
   );
 };
