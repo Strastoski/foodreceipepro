2e895b08650ddb8f6452f3cef78f4ee097da2f19
Strastoski <ezrawilliam98@gmail.com>
2024-01-22 11:02:54 +0700

    AddRecipe 75%

--- src/Page/AddRecipe.jsx
+++ src/Page/AddRecipe.jsx
@@ -10,7 +10,28 @@ const AddRecipe = () => {
 
       <input type="text" placeholder="Add Photo" className="addphoto" />
 
-      <input type="text" placeholder="Add Photo" className="titleadd" />
+      <input type="text" placeholder="Title" className="titleadd" />
+
+      <input type="text" placeholder="Ingredients" className="ingadd" />
+
+      <input type="text" placeholder="Video" className="titleadd" />
+
+      <div className="postadd">Post</div>
+
+      <footer className="footer">
+        <div className="footer-text">
+          <p className="footer-text-first">Eat, Cook, Repeat</p>
+          <p className="footer-text-second">
+            Share Your Best Recipe By Uploading Here !
+          </p>
+        </div>
+        <div className="footer3">
+          <a href="/">Product</a>
+          <a href="/">Company</a>
+          <a href="/">Learn More</a>
+          <a href="/">Get In Touch</a>
+        </div>
+      </footer>
     </div>
   );
 };
