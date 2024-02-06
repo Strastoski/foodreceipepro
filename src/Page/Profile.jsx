b38a4dc8cdf34d22811a2fc5b5f372446c985625
Strastoski <ezrawilliam98@gmail.com>
2024-02-06 15:25:38 +0700

    profile page fix container

--- src/Page/Profile.jsx
+++ src/Page/Profile.jsx
@@ -26,27 +26,7 @@ const Profile = () => {
       </div>
 
       <div className="ingredients-text-container">
-        <h1 className="gried-title">Ingredients</h1>
-        <div className="ing-text">
-          <p>- 2 eggs</p>
-          <p>- 2 tbsp mayonnaise</p>
-          <p>- 3 slices bread</p>
-          <p>- a little butter</p>
-          <p>- ⅓ carton of cress</p>
-          <p>
-            - 2-3 slices of tomato or a lettuce leaf and a slice of ham or
-            cheese
-          </p>
-          <p>- crisps, to serve</p>
-        </div>
-        <h1 className="videostep-title">Video Step</h1>
-        <div className="playbut"></div>
-        <div className="playbut"></div>
-        <div className="playbut"></div>
-        <div className="playbut"></div>
-
-        <input type="text" placeholder="Comment:" className="comments" />
-        <div className="send">Send</div>
+        <div className="3options-container"></div>
         <footer className="footer">
           <div className="footer-text">
             <p className="footer-text-first">Eat, Cook, Repeat</p>
